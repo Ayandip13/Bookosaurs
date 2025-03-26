3c3417acc01f1e6866e24b3c31dd1ff91f81b87c
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-03-26 17:22:14 +0530

    add user model and some register controller part

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,11 @@ import { ConnectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json()); //Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
-  ConnectDB()
+  ConnectDB();
 });
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,57 @@
 import express from "express";
+import User from "../models/user.model.js";
 
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
-  res.send("loggedin");
+  try {
+    const { email, username, password } = req.body;
+    if (!email || !password || !username) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
+    }
+
+    if (username.length < 4) {
+      return res
+        .status(400)
+        .json({ message: "Username should be at least 4 character long" });
+    }
+
+    //check if user already exist or not
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email is already exists" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    // get random avatar
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`
+
+    const user = new User({
+      email,
+      username,
+      password,
+      profileImage
+    })
+
+    await user.save()
+
+
+
+
+
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/register", async (req, res) => {
