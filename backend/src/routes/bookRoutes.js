bff33fbc5110b114038f75108390f62e1a534e88
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-03-29 12:57:01 +0530

    add minor bug fixes and add pagination

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.post("/", protectRoute, async (req, res) => {
   try {
-    req.user
     const { title, caption, rating, image } = req.body;
     if (!image || !title || !caption || !rating)
       return res.status(400).json({ message: "Please Provide all fields" });
@@ -23,13 +22,42 @@ router.post("/", protectRoute, async (req, res) => {
       caption,
       rating,
       image: imageUrl,
-      // user: req.user._id
+      user: req.user._id,
     });
     await newBook.save();
     res.status(201).json(newBook);
   } catch (error) {
     console.log("Error creating book ", error);
     res.status(500).json({ message: error.message });
+  }
+});
+
+//pagination => infinite loading...
+
+router.get("", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    //Book.find() is a Mongoose query that retrieves all documents from the books collection.
+    const books = await Book.find()
+      .sort({ createdAt: -1 }) // Sort books by newest first (descending order)
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage"); //Retrieves only the "username" and "profileImage" fields from the user(field from Book schema) collection.
+
+    const totalBooks = await Book.countDocuments();
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+  } catch (error) {
+    console.log("Error in get all books route", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
