5cfa3322e778b40ad7792c73662f238fb997bb14
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-04-05 23:59:18 +0530

    add create screen

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,13 +1,59 @@
-import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { Tabs } from "expo-router";
+import COLORS from "../../constants/colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="create" />
-      <Tabs.Screen name="rofile" />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS?.primary,
+        headerTitleStyle: {
+          color: COLORS.textPrimary,
+          fontWeight: "600",
+        },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.cardBackground,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          paddingTop: 3,
+          height: 52 + insets.bottom,
+          paddingBottom: insets.bottom,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home-outline" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="add-circle-outline" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person-outline" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import styles from "../../assets/styles/create.styles";
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [image, setImage] = useState(null);
+  const [imageBase64, setImageBase64] = useState(null); //Base64 is a way to turn images into text so they can be easily sent over the internet. It's like translating a picture into words that computers can send in messages.
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const pickImage = async () => {};
+
+  const handleSubmit = async () => {};
+
   return (
-    <View>
-      <Text>create tab</Text>
-    </View>
-  )
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.scrollViewStyle}
+      >
+        <View style={styles.card}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Add Book Recomendation</Text>
+            <Text style={styles.subtitle}>
+              Share your favorite read with others
+            </Text>
+          </View>
+          <View style={styles.form}>
+            <View style={styles.formGroup}></View>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
